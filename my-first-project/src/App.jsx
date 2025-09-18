b4c27ed14fada903b9a3f67af8f922ceb8573658
RafiaBonna <rafiaakterbonna@gmail.com>
2025-09-18 12:58:31 +0600

    my

--- my-first-project/src/App.jsx
+++ my-first-project/src/App.jsx
@@ -1,34 +1,47 @@
 import React from 'react';
-import FirstPage from './FirstPage';
 import Secondpage from './Secondpage';
+import TabPage from './TabPage';
 
 const App = () => {
   return (
     <div>
-      <FirstPage/>
-    
-    <div>
-      <form>
-    <div className="mb-3">
-      <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-      <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-      <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-    </div>
-    <div className="mb-3">
-      <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-      <input type="password" className="form-control" id="exampleInputPassword1" />
-    </div>
-    <div className="mb-3 form-check">
-      <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-      <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-    </div>
-    <button type="submit" className="btn btn-primary">Submit</button>
-  </form>
-</div>
-
-
+      <TabPage/>
       <h2>Good Days are coming!</h2>
       <Secondpage/>
+      <div>
+ <table className="table table-success table-striped">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>John</td>
+      <td>Doe</td>
+      <td>@social</td>
+    </tr>
+  </tbody>
+</table>
+
+
+        </div>
     </div>
   );
 };
