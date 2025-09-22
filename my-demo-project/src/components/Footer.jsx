d97e2ecf6e3072c9f41ee10d2dd1151f3db5d685
RafiaBonna <rafiaakterbonna@gmail.com>
2025-09-22 11:17:16 +0600

    my

--- my-demo-project/src/components/Footer.jsx
+++ my-demo-project/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <footer className="bg-dark text-white text-center p-2 mt-4">
-      <p>© 2025 My Website | All rights reserved</p>
+      <p>© 2025 My Website | All rights reserved by Rafia Hawlader Bonna</p>
     </footer>
   );
 }
