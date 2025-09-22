bc1e9a64d7ebaf66dad6dae618380d9adb8550e7
RafiaBonna <rafiaakterbonna@gmail.com>
2025-09-22 10:24:14 +0600

    my

--- my-demo-project/src/components/Sidebar.jsx
+++ my-demo-project/src/components/Sidebar.jsx
@@ -3,9 +3,9 @@ function Sidebar() {
     <div className="bg-light p-3" style={{ minHeight: "400px" }}>
       <h5>Sidebar</h5>
       <ul className="list-unstyled">
-        <li><a href="#">Link 1</a></li>
-        <li><a href="#">Link 2</a></li>
-        <li><a href="#">Link 3</a></li>
+        <li><a href="#">Page1</a></li>
+        <li><a href="#">Page2</a></li>
+        <li><a href="#">Page3</a></li>
       </ul>
     </div>
   );
